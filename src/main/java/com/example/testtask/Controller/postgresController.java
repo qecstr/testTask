59318968d6e210fdc8e81f5bd31b24ea47a2e8fa
qecstr <yessenov.zhassulan@gmail.com>
2023-10-25 04:25:27 +0300

    finished with postgreSql part

--- src/main/java/com/example/testtask/Controller/postgresController.java
+++ src/main/java/com/example/testtask/Controller/postgresController.java
@@ -1,12 +1,19 @@
 package com.example.testtask.Controller;
 
 import com.example.testtask.DTO.phoneDTO;
+import com.example.testtask.Entitty.Filter;
 import com.example.testtask.Entitty.Phone;
 import com.example.testtask.Repository.postgresRepo;
 import com.example.testtask.Service.entityToDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,15 +22,52 @@ public class postgresController {
     postgresRepo pR;
 
     entityToDTO eTD;
-    @PostMapping("/save")
-    public void save(){
 
-    }
-    @GetMapping("/get")
+    @GetMapping("/getById")
     @ResponseBody
-    public phoneDTO get(@RequestBody Phone p){
+    public phoneDTO getById(@RequestParam Long id){
+        return eTD.mapToDTO(pR.findPhoneById(id));
+    }
+    @GetMapping("/getByNumber")
+    @ResponseBody
+    public phoneDTO getByPhoneNumber(@RequestParam Long phoneNumber){
+        return eTD.mapToDTO(pR.findPhoneByPhoneNumber(phoneNumber));
+    }
+    @PutMapping("/updateById")
+    public Phone updateById( Phone p){
+       Phone entity = pR.findPhoneById(p.getId());
+        if(p.getName().isEmpty()){p.setName(entity.getName());}
+        if(p.getPhoneNumber() == null){p.setPhoneNumber(entity.getPhoneNumber());}
+        if(p.getSecondNumber() == null){p.setSecondNumber(entity.getSecondNumber());}
+        if(p.getDateOfBirth() == null){p.setDateOfBirth(entity.getDateOfBirth());}
 
+       p.setDateOfCreation(new Timestamp(System.currentTimeMillis()));
+        return pR.save(p);
+    }
+    @PutMapping("/updateByPhoneNumber")
+    public Phone updateByPhoneNumber( Phone p){
+        p.setId(pR.findPhoneByPhoneNumber(p.getPhoneNumber()).getId());
+        Phone entity = pR.findPhoneById(p.getId());
+        if(p.getName().isEmpty()){p.setName(entity.getName());}
+        if(p.getSecondNumber() == null){p.setSecondNumber(entity.getSecondNumber());}
+        if(p.getDateOfBirth() == null){p.setDateOfBirth(entity.getDateOfBirth());}
+        p.setDateOfCreation(new Timestamp(System.currentTimeMillis()));
+        return pR.save(p);
+    }
+    @DeleteMapping("/deleteById")
+    public void deleteById(@RequestParam Long id){
+        pR.deleteById(id);
+    }
+    @DeleteMapping("/deleteByPhoneNumber")
+    public void deleteByPhoneNumber(@RequestParam Long phoneNumber){
 
-       return eTD.mapToDTO( pR.findPhoneByIdOrPhoneNumberOrSecondNumber(p.getId(),p.getPhoneNumber(),p.getSecondNumber()));
+        pR.deleteById(pR.findPhoneByPhoneNumber(phoneNumber).getId());
+    }
+
+    @GetMapping("/getList")
+    public List<Phone> getList(Filter f){
+        Pageable request = PageRequest.of(f.getOffset(), f.getLimit());
+        Page<Phone> allPhone = pR.findAll(request);
+        return allPhone.stream().toList();
     }
 }
--- src/main/java/com/example/testtask/Repository/postgresRepo.java
+++ src/main/java/com/example/testtask/Repository/postgresRepo.java
@@ -1,21 +1,28 @@
 package com.example.testtask.Repository;
 
 import com.example.testtask.Entitty.Phone;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface postgresRepo extends JpaRepository<Phone,Long> {
-        Phone findPhoneByIdOrPhoneNumberOrSecondNumber(Long id,Long phoneNumber,Long secondNumber);
+
+        Phone findPhoneById(Long id);
+        Phone findPhoneByPhoneNumber(Long phoneNumber);
+        Phone save(Phone p);
+        void deleteById(Long id);
 
 
-        void deleteByIdOrPhoneNumberOrSecondNumber(Long id, Long phoneNumber, Long secondNumber);
+
+      /*  @Query(nativeQuery = true ,value = "SELECT p FROM Phone p LIMIT ?1 OFFSET ?2")
+        List<Phone> findAll(int limit, int offset);*/
+      Page<Phone> findAll(Pageable pageable);
 
 
-       // List<Phone> findAll(int limit,int offset);
 
 }
